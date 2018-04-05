9e0d6c76b1c3082a120ec84c20f4f8df15de877e
Jean Hibbert <jeanh2010@hotmail.com>
2018-04-06 00:29:39 +0100

    Add project files.

---
+++ Dcs.DynamicWcfServiceConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dcs.DynamicWcfServiceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
